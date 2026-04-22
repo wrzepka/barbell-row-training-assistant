450195393d1ee3bfc43696c3c61d4ca2d6b7db0b
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-04-23 00:03:57 +0200

    chore(main): create skeleton of the main file

--- main.py
+++ main.py
@@ -1,2 +1,40 @@
+from PySide6.QtWidgets import QApplication
+import sys
+
+
+from app.core.config import VOSK_DIR, PIPER_DIR
+
+
+def check_env():
+    """
+    Sprawdza, czy wszystkie krytyczne komponenty istnieją.
+    Funkcja bazuje na pliku '/app/core/config.py'.
+    """
+
+    # Weryfikacja modeli
+    if not VOSK_DIR.exists() or not any(VOSK_DIR.iterdir()):
+        print(f"Brak modelu Vosk(STT) w {VOSK_DIR}!")
+
+    if not PIPER_DIR.exists() or not any(PIPER_DIR.iterdir()):
+        print(f"Brak modelu Piper(TTS) w {PIPER_DIR}!")
+
+    # TODO: Sprawdzanie bazy danych
+
+
+def main():
+    print("==-URUCHAMIANIE-==")
+
+    check_env()
+
+    # Inicjalizacja PySide6
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+
+    print("Ładowanie GUI")
+    # TODO: GUI
+
+    print("Aplikacja gotowa do działania!")
+    sys.exit(app.exec()) # Event loop
+
 if __name__ == "__main__":
-    pass
+    main()
