bae1195e9829aeb7498a6745d24523e7a2c87fc5
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-05-12 21:07:00 +0200

    feat: add ScreenModes enumerator

--- app/const.py
+++ app/const.py
@@ -9,3 +9,12 @@ class Screen(IntEnum):
     LOBBY = 0
     TRAINING = 1
     HISTORY = 2
+
+class ScreenModes(IntEnum):
+    """
+    Enumerator z identyfikatorami trybów każdego z widoków QStackedWidget.
+    Każdy widok składa się z szkieletu (używanego podczas ładowania widoku) i realnego widoku.
+    """
+
+    SKELETON = 0
+    REAL = 1
