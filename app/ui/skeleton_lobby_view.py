99309c92fe3ad5fdbc522ac706906724d3569c09
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-05-27 12:56:22 +0200

    fix: modify lobby skeleton layout

--- app/ui/skeleton_lobby_view.py
+++ app/ui/skeleton_lobby_view.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout
 from app.ui.shimmer_block import ShimmerBlock
 
 class SkeletonLobbyView(QWidget):
@@ -8,10 +9,24 @@ class SkeletonLobbyView(QWidget):
 
     def __init__(self):
         super().__init__()
-        layout = QVBoxLayout(self)
-        layout.setContentsMargins(10, 10, 10, 10)
-        layout.setSpacing(10)
+        main_layout = QVBoxLayout(self)
+        main_layout.setContentsMargins(50, 30, 75, 50)
+        main_layout.setSpacing(30)
 
-        # blok dla tekstu
-        layout.addWidget(ShimmerBlock(height=40))
-        layout.addStretch()
+        main_layout.addWidget(ShimmerBlock(height=40, width=300), alignment=Qt.AlignmentFlag.AlignCenter)
+
+        columns_layout = QHBoxLayout()
+        columns_layout.setSpacing(40)
+        left_column = ShimmerBlock()
+        columns_layout.addWidget(left_column, stretch=4)
+
+        right_column_layout = QVBoxLayout()
+        right_column_layout.setSpacing(20)
+
+        weekly_result_card = ShimmerBlock(height=525)
+        right_column_layout.addWidget(weekly_result_card, stretch=6)
+        last_training_card = ShimmerBlock()
+        right_column_layout.addWidget(last_training_card, stretch=4)
+
+        columns_layout.addLayout(right_column_layout, stretch=6)
+        main_layout.addLayout(columns_layout)
