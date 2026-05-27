6d1c01733d3a92403869ddfa9089373cfaac8fb6
Filip Tomczyk <243439700+FTomczyk@users.noreply.github.com>
2026-05-27 17:05:07 +0200

    fix/background color (#25)
    * fix/background color
    
    * update text

--- app/ui/lobby_view.py
+++ app/ui/lobby_view.py
@@ -37,7 +37,7 @@ class LobbyView(BaseView):
         - prawy panel z wykresem tygodniowym i ostatnią sesją
         """
         # Tytuł strony
-        self.title_label = QLabel("BARBEL ROW TRENING")
+        self.title_label = QLabel("Witaj z powrotem!")
         self.title_label.setObjectName("lobbyTitle")
         self.title_label.setAlignment(Qt.AlignCenter)
 
