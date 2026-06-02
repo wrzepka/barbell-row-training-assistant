a5e0b83ddbcf8c1c40d78b5cdbccf69ccd7ab4e4
Bartekzywicki <84078181+Bartekzywicki@users.noreply.github.com>
2026-06-02 21:33:46 +0200

    Refactor/added list with keywords instead of wall of "or's"

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -98,16 +98,17 @@ class MainWindow(QMainWindow):
         """
         print(f"[{text}]")
 
-        # Można uprościć kod tutaj bazując na sygnałach, które będą przechwtywane w VoiceManager
-        # przykładowa logika reagowania na komendy
-        if "trening" in text.lower() or "terenie" in text.lower() or "trendy" in text.lower() or "rynek" in text.lower() or "premier" in text.lower() or "trend" in text.lower() or "teren" in text.lower():
+
+        training_list = ["trening","terenie","trendy","rynek","premier","technik","trend","teren"]
+        history_list =["historia","historie","zdrowia","historię"]
+        lobby_list = ["lobby","po lody","loty","lampki","lotu","lody","robi","logiki","roku","nowy","lobbing","nogi","lubi","lampy","login","lori","wątpię","lot","start","stan"]
+        if any(word in text.lower() for word in training_list):
             self.switch_page(1)  #przełącza na trening
             self.voice_manager.speak("Przechodzę do treningu.")
-
-        elif "historia" in text.lower() or "historie" in text.lower() or "zdrowia" in text.lower() or "historię" in text.lower():
+        elif any(word in text.lower() for word in history_list):
             self.switch_page(2)  #przełącza na historie
             self.voice_manager.speak("Oto twoja historia treningów.")
-        elif "lobby" in text.lower() or "start" in text.lower() or "po lody" in text.lower() or "loty" in text.lower() or "lampki" in text.lower() or "lotu" in text.lower() or "lody" in text.lower() or "robi" in text.lower() or "nowi" in text.lower() or "logiki" in text.lower() or "roku" in text.lower() or "nowy" in text.lower() or "lobbing" in text.lower() or "nogi" in text.lower() or "lubi" in text.lower() or "lampy" in text.lower() or "login" in text.lower() or "lori" in text.lower() or "wątpie" in text.lower() or "lot" in text.lower() or "stan" in text.lower():
+        elif any(word in text.lower() for word in lobby_list):
             self.switch_page(0) #przełąccza na start
             self.voice_manager.speak("Przechodzę na stronę startową")
 
